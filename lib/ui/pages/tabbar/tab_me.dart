4ba6dace1ff57e775bfe6fa9f61fdd09abf31032
hech <402826438@qq.com>
2020-06-29 15:49:47 +0800

    input控件封装

--- lib/ui/pages/tabbar/tab_me.dart
+++ lib/ui/pages/tabbar/tab_me.dart
@@ -1,6 +1,8 @@
 import 'package:xy_wallet/common/Base/base_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:xy_wallet/ui/pages/tabbar/tab_community.dart';
+import 'package:xy_wallet/ui/widgets/common_button.dart';
+import 'package:xy_wallet/ui/widgets/common_input.dart';
 
 class TabMe extends BaseWidget {
   @override
@@ -10,7 +12,6 @@ class TabMe extends BaseWidget {
 }
 
 class Pages extends BaseWidgetState<TabMe> {
-
   @override
   Widget getBaseView(BuildContext context) {
     return super.getBaseView(context);
@@ -18,12 +19,32 @@ class Pages extends BaseWidgetState<TabMe> {
 
   @override
   Widget buildBaseWidget(BuildContext context) {
-    return Row(
+    return Column(
       children: <Widget>[
-        RaisedButton(onPressed: () {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) => TabCommunity()));
-        })
+        // RaisedButton(onPressed: () {
+        //   Navigator.of(context)
+        //       .push(MaterialPageRoute(builder: (context) => TabCommunity()));
+        // }),
+        CommonInput(
+          placeholder: '1231231231',
+          title: '测试测试',
+        ),
+        CommonInput(
+          placeholder: '1231231231',
+          title: '测试测试',
+        ),
+        CommonInput(
+          placeholder: '1231231231',
+          title: '测试测试',
+          controller: TextEditingController(text: "初始化的"),
+          onChanged: (text) {
+            print("++++" + text);
+          }
+        ),
+
+        // Text('12131231'),
+        // Text('12131231'),
+        // Text('12131231'),
       ],
     );
   }
---
+++ lib/ui/widgets/common_input.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:xy_wallet/common/helper/resource_helper.dart';
+
+
+class CommonInput extends StatefulWidget{
+
+  final String placeholder;
+  final String title;
+  final bool autofocus;
+  final TextEditingController controller;
+  final TextInputAction textInputAction;
+  final FocusNode focusNode;
+  final ValueChanged<String> onChanged;
+  final TextInputType keyboardType;
+
+  CommonInput({
+    Key key,
+    this.placeholder,
+    this.title,
+    this.autofocus,
+    this.keyboardType,
+    this.controller,
+    this.textInputAction,
+    this.focusNode,
+    this.onChanged
+
+  }) : super(key: key);
+
+  @override
+  CommonInputState createState() => CommonInputState();
+
+}
+
+
+class CommonInputState extends State<CommonInput> {
+
+  @override
+  Widget build(BuildContext context) {
+    // double width = MediaQuery.of(context).size.width;
+    // double height = MediaQuery.of(context).size.height;
+
+
+      return Container(
+        alignment: Alignment.center,
+        // height: 106,
+        // width: width,
+        margin: EdgeInsets.all(3.0),
+      
+        child: Padding(
+          padding: const EdgeInsets.all(0.0),
+          child:Stack(
+            children: <Widget>[
+              Image.asset(
+              ImageHelper.wrapAssets('inputbg.png'),
+              fit: BoxFit.fill,
+              ),
+
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25, 10, 25, 70),
+                child: Text(
+                  widget.title,
+                  
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(25, 45, 50, 0),
+                child: TextField(
+                  onChanged: widget.onChanged,
+                  
+                  controller: widget.controller,
+                  keyboardType: widget.keyboardType,
+                  style: TextStyle(color: Colors.black, fontSize: 17.0),
+                  decoration: InputDecoration(
+                    hintText: widget.placeholder,
+                    hintStyle: TextStyle(color: Colors.lightBlue, fontSize: 17.0),
+                    border: InputBorder.none,
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Color.fromARGB(0, 0, 0, 0),),  
+                    )
+                  )
+                ),
+              
+              ),
+
+
+            ],
+          ) 
+          
+
+          
+          
+        ),
+        // color: Colors.blue,
+      );
+  }
+}
+
+// 文本框border
+InputBorder TextFieldDecorationBorder() {
+  return new OutlineInputBorder(
+      borderSide: new BorderSide(color: Color(0xffd2d2d2), width: 0.6),
+      borderRadius: new BorderRadius.circular(6.0)
+  );
+}
