cc1e8ee98a8b022a0730fa180486181f95ac1c19
hech <402826438@qq.com>
2020-06-30 09:38:22 +0800

    文件提交

---
+++ lib/ui/widgets/tabWallet_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:xy_wallet/common/helper/resource_helper.dart';
+
+class TabWalletBt extends StatelessWidget {
+
+  final String imageName;
+  final String title;
+  
+
+
+  TabWalletBt({
+    Key key,
+    this.imageName,
+    this.title,
+  }) : super(key: key);
+  
+  Widget build(BuildContext context) {
+    return Stack(children: <Widget>[
+      Image.asset(
+        ImageHelper.wrapAssets(imageName),
+        fit: BoxFit.fill,
+        width: 50,
+        height: 50,
+      ),
+      Container(
+        alignment: Alignment.center,
+        width: 50,
+        padding: EdgeInsets.only(top: 40),
+        child: Text(
+          title,
+          style: new TextStyle(color: Color(0xFF3FA0B3), fontSize: 11),
+        ),
+      ),
+    ]);
+  }
+}
