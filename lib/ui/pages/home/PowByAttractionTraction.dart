03a63ddc09dba0dee1d1db4c31be7a6345e49859
Jone <jone.work@outlook.com>
2020-07-13 16:01:59 +0800

    折线图先这样

--- lib/ui/pages/home/PowByAttractionTraction.dart
+++ lib/ui/pages/home/PowByAttractionTraction.dart
@@ -1,5 +1,8 @@
+import 'dart:collection';
+
 import 'package:xy_wallet/common/Base/base_widget.dart';
 import 'package:flutter/material.dart';
+
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:xy_wallet/common/themes.dart';
 
@@ -9,61 +12,64 @@ class PowByAttractionTractionPage extends BaseWidget {
 }
 
 class _State extends BaseWidgetState<PowByAttractionTractionPage> {
-  List<charts.Series> _seriesList;
-
   @override
   void initState() {
     super.initState();
-    _seriesList = _createSampleData();
   }
 
   @override
   Widget buildBodyWidget(BuildContext context) {
-    //  final ticks=charts.StaticOrdinalTickProviderSpec([
-    //     charts.TickSpec(
-    //         '2018',
-    //         label: '2018年',
-    //         style: new charts.TextStyleSpec(//可对x轴设置颜色等
-    //             color: new charts.Color(r: 0x4C, g: 0xAF, b: 0x50)))
-    //     ),
-    //     charts.TickSpec(
-    //         '2019',
-    //         label: '2019年',
-    //         style: new charts.TextStyleSpec(//可对x轴设置颜色等
-    //             color: new charts.Color(r: 0x4C, g: 0xAF, b: 0x50)))
-    //     ),
-    // ]);
-    return charts.LineChart(
-      _createSampleData(),
-      animate: true,
-      defaultRenderer: new charts.LineRendererConfig(includePoints: true),
-      // domainAxis: new charts.OrdinalAxisSpec(
-      //   tickProviderSpec: ticks,
-      // ),
+    final ticks = new charts.StaticNumericTickProviderSpec([
+      new charts.TickSpec(233,
+          label: '2018年',
+          style: new charts.TextStyleSpec(
+              //可对x轴设置颜色等
+              color: new charts.Color(r: 0x4C, g: 0xAF, b: 0x50))),
+      new charts.TickSpec(22,
+          label: '2019年',
+          style: new charts.TextStyleSpec(
+              //可对x轴设置颜色等
+              color: new charts.Color(r: 0x4C, g: 0xAF, b: 0x50))),
+    ]);
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xFF082E41),
+      ),
+      child: charts.LineChart(
+        _createSampleData(),
+        animate: true,
+        defaultInteractions: true,
+        secondaryMeasureAxis: charts.NumericAxisSpec(
+          showAxisLine: false,
+          tickProviderSpec: ticks,
+        ),
+        defaultRenderer: new charts.LineRendererConfig(
+          includePoints: true,
+          radiusPx: 4,
+        ),
+      ),
     );
   }
 
-  /// Create one series with sample hard coded data.
   static List<charts.Series<LinearSales, int>> _createSampleData() {
-    final data = [
+    final tableSalesData = [
+      // new LinearSales(0, 50),
+      // new LinearSales(1, 100),
+      // new LinearSales(2, 200),
+      // new LinearSales(3, 150),
       new LinearSales(0, 5),
       new LinearSales(1, 25),
-      new LinearSales(2, 100),
-      new LinearSales(3, 75),
     ];
-
+    final color = ThemeColors.primaryFgColor;
     return [
-      new charts.Series<LinearSales, int>(
-        id: 'Sales',
-        colorFn: (_, __) => charts.Color(
-          r: ThemeColors.accentColor.red,
-          g: ThemeColors.accentColor.green,
-          b: ThemeColors.accentColor.blue,
-        ),
+      charts.Series<LinearSales, int>(
+        id: 'Tablet',
+        colorFn: (_, __) => charts.Color(r: color.red, g: color.green, b: color.blue, a: 150),
         domainFn: (LinearSales sales, _) => sales.year,
         measureFn: (LinearSales sales, _) => sales.sales,
-        data: data,
-      )
+        data: tableSalesData,
+      ),
     ];
   }
 }
--- lib/ui/pages/wallet/test.dart
+++ lib/ui/pages/wallet/test.dart
@@ -1,10 +1,7 @@
-import 'package:charts_flutter/flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:xy_wallet/common/base/base_widget.dart';
 import 'package:xy_wallet/common/base/widgets/view_loading_shimmer.dart';
 import 'package:xy_wallet/ui/widgets/shimmer/transaction_details_shimmer.dart';
-
-import 'package:charts_flutter/flutter.dart' as charts;
 
 class TestPage extends BaseWidget {
   @override
@@ -12,38 +9,16 @@ class TestPage extends BaseWidget {
 }
 
 class _TestPageState extends BaseWidgetState<TestPage> {
-  List<charts.Series> _seriesList;
-
   @override
   void initState() {
     super.initState();
-    _createSampleData();
   }
 
   @override
-  Widget buildBodyWidget(BuildContext context) {
-    return charts.LineChart(_seriesList, animate: true, defaultRenderer: new charts.LineRendererConfig(includePoints: true));
-  }
+  Widget buildBodyWidget(BuildContext context) {}
 
   /// Create one series with sample hard coded data.
-  static List<charts.Series<LinearSales, int>> _createSampleData() {
-    final data = [
-      new LinearSales(0, 5),
-      new LinearSales(1, 25),
-      new LinearSales(2, 100),
-      new LinearSales(3, 75),
-    ];
 
-    return [
-      new charts.Series<LinearSales, int>(
-        id: 'Sales',
-        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-        domainFn: (LinearSales sales, _) => sales.year,
-        measureFn: (LinearSales sales, _) => sales.sales,
-        data: data,
-      )
-    ];
-  }
 }
 
 /// Sample linear data type.
